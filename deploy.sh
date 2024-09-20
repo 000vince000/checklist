ab0977d89e93368271e33764a0f6278adca1c58f
000vince000 <00vince00@gmail.com>
2024-09-20 14:31:17 -0400

    debug

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:000vince000/checklist.git main:gh-pages
+git push -f git@github-000vince000:000vince000/checklist.git main:gh-pages
 
 cd -
