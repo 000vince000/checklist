7b33a20737eb97e42aeb6d290219bd0edb47b88e
Vincent Chen <00vince00@gmail.com>
2025-04-08 15:41:27 -0400

    attempt to fix deploy prob where console log says GET https://000vince000.github.io/checklist/%PUBLIC_URL%/bundle.js net::ERR_ABORTED 400 (Bad Request)

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const path = require('path');  // Add this line
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // Call dotenv and it will return an Object with a parsed key 
-const env = dotenv.config().parsed;
+const env = dotenv.config().parsed || {};
+
+// Determine if we're in production mode
+const isProduction = process.env.NODE_ENV === 'production';
 
 // Reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
@@ -29,11 +32,16 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: isProduction ? '/checklist/' : '/', // Set the publicPath for GitHub Pages
   },
   plugins: [
-    new webpack.DefinePlugin(envKeys),
+    new webpack.DefinePlugin({
+      ...envKeys,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
+      inject: true
     }),
   ],
   devServer: {
